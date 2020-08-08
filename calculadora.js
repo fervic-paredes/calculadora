c99558bf1203546b736f3d4a52cb235c27ebfa54
fervic paredes <fervicparedes@gmail.com>
2020-08-08 03:28:28 -0400

    ending

--- calculadora.js
+++ calculadora.js
@@ -1,183 +1,219 @@
 var operandoa;
 var operandob;
-var operacion;
+var operation;
 var memories;
+var state = true; 
+var copy;
 
 function calculadora(){
-    
- //variables
- var calculator__display = document.getElementById("calculator__display");
- var off = document.getElementById("off");
- var porcentaje = document.getElementById("%");
- var raiz = document.getElementById("squareroot");
- var flecha = document.getElementById("flecha");
- var reset = document.getElementById("ON/C");
- var siete = document.getElementById("siete");
- var ocho = document.getElementById("ocho");
- var nueve = document.getElementById("nueve");
- var multiplicacion = document.getElementById("X");
- var division = document.getElementById("/");
- var cuatro = document.getElementById("cuatro");
- var cinco = document.getElementById("cinco");
- var seis = document.getElementById("seis");
- var menos = document.getElementById("menos");
- var MRC = document.getElementById("MRC");
- var uno = document.getElementById("uno");
- var dos = document.getElementById("dos");
- var tres = document.getElementById("tres");
- var suma = document.getElementById("suma");
- var less__memories = document.getElementById("M-");
- var cero = document.getElementById("cero");
- var punto = document.getElementById("punto");
- var igual = document.getElementById("igual");
- var plus__memories = document.getElementById("M+");
-  
+
+    //variables
+    var calculator__display = document.getElementById("calculator__display");
+    var off = document.getElementById("off");
+    var percent = document.getElementById("%");
+    var raiz = document.getElementById("squareroot");
+    var flecha = document.getElementById("flecha");
+    var reset__encendido = document.getElementById("ON/C");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var multiplicacion = document.getElementById("X");
+    var division = document.getElementById("/");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var menos = document.getElementById("menos");
+    var MRC = document.getElementById("MRC");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var suma = document.getElementById("suma");
+    var less__memories = document.getElementById("M-");
+    var cero = document.getElementById("cero");
+    var period = document.getElementById("punto");
+    var igual = document.getElementById("igual");
+    var plus__memories = document.getElementById("M+");
      
- //eventos
-    uno.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "1";
-        console.log("hola mundo");
-    }
+  
+ //000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 
+    //buttons
 
-    dos.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "2";
-    }
+    
+        uno.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "1";
+                copy = "fervic paredes";
+                console.log("fervic paredes".substr(copy.indexOf("e"), copy.length));
 
-    tres.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "3";
-    }
+                
+            }
+        }
 
-    cuatro.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "4";
-    }
+        dos.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "2";
+            }
+        }
 
-    cinco.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "5";
-    }
+        tres.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "3";
+            }
+        }
 
-    seis.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "6";
-    }
+        cuatro.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "4";
+            }
+        }
 
-    siete.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "7";
-    }
+        cinco.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "5";
+            }
+        }
 
-    ocho.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "8";
-    }
+        seis.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "6";
+            }
+        }
 
-    nueve.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "9";
-    }
+        siete.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "7";
+            }
+        }
 
-    cero.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent + "0";
-    }
+        ocho.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "8";
+            }
+        }
 
-    reset.onclick = function(e){
-        calculator__display.textContent = "";
-        operandoa = 0;
-        operandob = 0;
-        operacion = "";
-    }
+        nueve.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "9";
+            }
+        }
 
-    MRC.onclick = function(e){
-        calculator__display.textContent = memories;
-    }
+        cero.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + "0";
+            }
+        }
 
-    less__memories.onclick = function(e){
-        memories = -(calculator__display.textContent);
-        limpiar();
-    }
+        period.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent + ".";
+            }
+        }
 
-    plus__memories.onclick = function(e){
-        memories = calculator__display.textContent;
-        limpiar();
-    }
+        MRC.onclick = function(e){
+            if (state){
+                calculator__display.textContent = memories;
+            }
+        }
 
-    flecha.onclick = function(e){
-        calculator__display.textContent = calculator__display.textContent.slice(1);
+        less__memories.onclick = function(e){
+            if (state){
+                memories = -(calculator__display.textContent);
+                limpiar();
+            }
+        }
+
+        plus__memories.onclick = function(e){
+            if (state){
+                memories = calculator__display.textContent;
+                limpiar();
+            }
+        }
+
+        flecha.onclick = function(e){
+            if (state){
+                calculator__display.textContent = calculator__display.textContent.slice(1);
+            }
+        }
+    
+
+    reset__encendido.onclick = function(e){
+        if (state){
+            limpiar();
+            clear__all();
+            state = true;
+        }
     }
 
     off.onclick = function(e){
-        reset();
-        while (true){
+        if (state){
             limpiar();
-            if (reset.onclick){
-                break;
-            }
+            clear__all();
+            state = false;
         }
     } 
- 
- 
- 
- 
- 
- 
- 
-     
- 
+    
+    
  //000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-    //operaciones
+    //operationes
     suma.onclick = function(e){
         operandoa = calculator__display.textContent;
-        operacion = "+";
+        operation = "+";
         limpiar();
     }
  
     menos.onclick = function(e){
         operandoa = calculator__display.textContent;
-        operacion = "-";
+        operation = "-";
         limpiar();
     }
  
     division.onclick = function(e){
         operandoa = calculator__display.textContent;
-        operacion = "/";
+        operation = "/";
         limpiar();
     }
  
     multiplicacion.onclick = function(e){
         operandoa = calculator__display.textContent;
-        operacion = "*";
+        operation = "*";
         limpiar();
     }
  
     raiz.onclick = function(e){
         operandoa = calculator__display.textContent;
-        operacion = "raiz";
+        operation = "raiz";
         limpiar();
         resolver();
     }
- 
-    reset.onclick = function(e){
-        calculator__display.textContent = "";
-        operandoa = 0;
-        operandob = 0;
-        operacion = "";
+
+    percent.onclick = function(e){
+        operandoa = calculator__display.textContent;
+        calculator__display.textContent = calculator__display.textContent + "%";
+        copy = calculator__display.textContent;
+        operation = "%";
     }
-         
+ 
     igual.onclick = function(e){
         operandob = calculator__display.textContent;
         resolver();
     }
-  
  
+    
+ //000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
     //funciones
     function limpiar(){
         calculator__display.textContent = "";
     }
  
-    function reset(){
+    function clear__all(){
         operandoa = 0;
         operandob = 0;
-        operacion = "";
+        operation = "%";
     }
      
     function resolver(){
         var res = 0;
-        switch(operacion){
+        switch(operation){
             case "+":
                 res = parseFloat(operandoa) + parseFloat(operandob);
                 break;
@@ -196,8 +232,12 @@ function calculadora(){
             case "raiz":
                 res = parseFloat(operandoa)**(1/2);
                 break;
+            
+            case "%":
+                operandob = calculator__display.textContent.substr(copy.indexOf("%") + 1, copy.length);
+                res = (parseFloat(operandoa) / 100) * parseFloat(operandob);
         }
-        reset();
+        clear__all();
         calculator__display.textContent = res;
     }
 }
